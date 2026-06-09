6b7c9e4d820d71329b82cd6c49b57adee25a75a1
deji <frdzhra24@gmail.com>
2026-06-09 12:13:18 +0700

    perbaikan modelling.py, workflow, dan conda

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,12 +6,9 @@ from sklearn.cluster import KMeans
 import joblib
 
 def train_model():
-    # 1. Mengatur rute dinamis (Dynamic Path)
-    # BASE_DIR saat ini adalah folder 'Membangun_model'
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     
-    # directory dataset hasil preprocessing berada di folder 'Customer_shopping_preprocessing' yang sejajar dengan 'Membangun_model'
-    data_path = os.path.abspath(os.path.join(BASE_DIR, "Customer_shopping_preprocessing", "shopping_trends_preprocessed.csv"))
+    data_path = os.path.join(BASE_DIR, "shopping_trends_preprocessed.csv")
         
     print(f"Membaca data hasil preprocessing dari:\n{data_path}\n")
     
@@ -19,23 +16,15 @@ def train_model():
         print("Peringatan: File dataset tidak ditemukan.")
         return
 
-    # Load dataset ke variabel df_clean
     df_clean = pd.read_csv(data_path)
     
-    # 2. Pengaturan MLflow Tracking Menggunakan SQLite Backend
     db_path = os.path.join(BASE_DIR, "mlflow.db")
-    
-    # Mengonversi format slash agar dikenali oleh SQLAlchemy URI
     sqlite_uri = f"sqlite:///{db_path.replace(os.sep, '/')}"
+    
     mlflow.set_tracking_uri(sqlite_uri)
-    
-    # Menentukan nama eksperimen di database
     mlflow.set_experiment("Submission Membangun Sistem Machine Learning - KMeans Clustering")
-    
-    # 3. Mengaktifkan Autolog MLflow
     mlflow.sklearn.autolog(log_models=True)
     
-    # 4. Melatih Model K-Means
     print("Memulai pencatatan ke MLflow Tracking UI (SQLite)...")
     with mlflow.start_run(run_name="KMeans_SQLite_Run") as run:
         n_clusters = 4
@@ -44,22 +33,15 @@ def train_model():
         model = KMeans(n_clusters=n_clusters, init='k-means++', random_state=42, n_init=10)
         model.fit(df_clean)
 
-        # Save model
-        # Membuat path ke folder 'model' yang sejajar dengan modelling.py
-        model_dir = os.path.abspath(os.path.join(BASE_DIR, "model"))
-        
-        # Pastikan folder 'model' ada (jika belum, buat otomatis)
+        model_dir = os.path.join(BASE_DIR, "model")
         if not os.path.exists(model_dir):
             os.makedirs(model_dir)
             
-        # Simpan model
         joblib.dump(model, os.path.join(model_dir, "kmeans_model.joblib"))
         print(f"\n[SUKSES] Model berhasil disimpan di: {os.path.join(model_dir, 'kmeans_model.joblib')}")
-        # ----------------------------------------------
-        
+    
         run_id = run.info.run_id
         print(f"\n[SUKSES] Model berhasil dilatih! Run ID: {run_id}")
-        print(f"Semua histori tracking berhasil dikunci di:\n{db_path}")
 
 if __name__ == "__main__":
     train_model()
